797fe8684f90e89ac6aed546210dade618b5859e
Naice-gif <nuwitonze98@gmail.com>
2019-07-21 18:29:17 +0200

    my nth commit

--- javas/index.js
+++ javas/index.js
@@ -1,4 +1,4 @@
-
+function forms(){
 
 var input = document.getElementById("birthdate").value;
 var d=new Date (input);
@@ -17,7 +17,7 @@ var cc= yy/100;
 var cc=d.getCentury();
 console.log(cc);
 
-if (document.getElementById("gender").checked){
+if (document.getElementById("mal", "fema").checked){
     var gender="male"
 }
 else{
@@ -27,74 +27,77 @@ else{
 var day= parseInt(dd+13*(mm+1)/5+yy+yy/4+cc/4+5*cc);
 
 day=day%7;
+var days=
+
 console.log(day);
 
+var akans=["Kwasi","Akosua","Kwadwo","Adwoa","Kwabena","Abena","Kwaku","Akua","Yaw","Yaa","Kofi","Afua","Kwame","Ama"]
 
-
-switch(ghana)
+switch(day)
 {
     case 0:
         if (day===0 && gender=== "male"){
-      document.getElementById("Akan-Name").innerHTML='Akosua';
+      document.getElementById("Akan-Name").innerHTML='akans[0]';
         }
         else {
-     document.getElementById("Akan-Name").innerHTML='Adwoa';
+     document.getElementById("Akan-Name").innerHTML='akans[1]';
     }
         break;
 
     case 1:
         if (day===1 && gender==="male"){
-            document.getElementById("Akan-Name").innerHTML='Kwadwo';
+            document.getElementById("Akan-Name").innerHTML='akans[2]';
         
         }
            else{
-            document.getElementById("Akan-Name").innerHTML='Akosua';;
+            document.getElementById("Akan-Name").innerHTML='akans[3]';;
         }
         break;
         
     case 2:
-          if (day===2 && gender==="male"){
-            document.getElementById("Akan-Name").innerHTML='Kwabena';;
+          if (day===2 && gender==="akans"){
+            document.getElementById("Akan-Name").innerHTML='akans[4]';;
           }
               else{
-            document.getElementById("Akan-Name").innerHTML='Adwoa';;
+            document.getElementById("Akan-Name").innerHTML='akans[5]';;
               }
               break;
 
       case 3:  
         if (day===3 && gender==="male"){
-            document.getElementById("Akan-Name").innerHTML='Kwaku';;
+            document.getElementById("Akan-Name").innerHTML='akans[6]';;
         }
             else{
-            document.getElementById("Akan-Name").innerHTML='Abenaa';;
+            document.getElementById("Akan-Name").innerHTML='akans[7]';;
         }
         break;
 
       case 4:
           if (day===4 && gender==="male"){
-            document.getElementById("Akan-Name").innerHTML='Yaw';;
+            document.getElementById("Akan-Name").innerHTML='akans[8]';;
           }
               else{
-            document.getElementById("Akan-Name").innerHTML='Yaa';
+            document.getElementById("Akan-Name").innerHTML='akans[9]';
           }     
           break;
 
       case 5:
           if ( day===5 && gender==="male"){
-            document.getElementById("Akan-Name").innerHTML='Kofi';;
+            document.getElementById("Akan-Name").innerHTML='akans[10]';;
           }
               else{
-            document.getElementById("Akan-Name").innerHTML='Afua';
+            document.getElementById("Akan-Name").innerHTML='akans[11]';
           } 
           break;
           
       case 6:
           if ( day===6 && gender==="male"){
-            document.getElementById("Akan-Name").innerHTML='Kwame';
+            document.getElementById("Akan-Name").innerHTML='akans[12]';
           }
 
               else{
-            document.getElementById("Akan-Name").innerHTML='Ama';
+            document.getElementById("Akan-Name").innerHTML='akans[13]';
           }      
         }
-    
+        return akans;
+      }
